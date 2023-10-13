74ca27705b9d98df8b8561192449189402ec038b
zerodark15 <93253159+zerodark15@users.noreply.github.com>
2023-10-13 18:31:01 +0800

    Update about

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
   menu.classList.toggle('open');
   icon.classList.toggle('open');
 }
+
+const tablinks = document.getElementsByClassName('tab-links');
+const tabcontents = document.getElementsByClassName('tab-contents');
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove('active-link');
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove('active-tab');
+  }
+  event.currentTarget.classList.add('active-link');
+  document.getElementById(tabname).classList.add('active-tab');
+}
